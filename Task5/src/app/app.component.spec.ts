7f0b682a48ae32b1255abdf7575b09fb4d1a3c66
SokolovskyiDmytro <vav2890@gmail.com>
2021-01-30 19:36:14 +0300

    Task5

--- Task5/src/app/app.component.spec.ts
+++ Task5/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Classwork2'`, () => {
+  it(`should have as title 'Task5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Classwork2');
+    expect(app.title).toEqual('Task5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Classwork2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Task5 app is running!');
   });
 });
