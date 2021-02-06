2207c181d269267859445e2e8fbb9708d5b88e30
SokolovskyiDmytro <vav2890@gmail.com>
2021-02-06 13:51:09 +0300

    classwork5

---
+++ classwork5/src/app/config/api.ts
@@ -0,0 +1,5 @@
+import {environment} from '../../environments/environment';
+
+export const URL = {
+  cars: `${environment.API}/cars`
+};
---
+++ classwork5/src/app/models/car.interface.ts
@@ -0,0 +1,6 @@
+export interface ICar {
+  model: string;
+  year: number;
+  id: number;
+  brand: string;
+}
